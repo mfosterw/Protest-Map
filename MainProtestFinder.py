3bfd1c45c73799a33fad9279ee4346eca0acda36
readjfb <readjfb@aol.com>
2020-06-05 00:39:29 -0500

    Added DBManager.py, MainProtestFinder.py, ProtestChicagoParser.py. DBManager manages SQLite3 protest database, and serves as intermediary. Read the file, it has comments describing what each func does. MainProtestFinder.py is intended to be the main file of the parsers. All parser will be called from it, and it will hold the DBmanager object. ProtestChicagoParser is the first webpage parser made so far. It can serve as a template for future parsers, though it is by no means optimal, and can be substantially improved.

---
+++ MainProtestFinder.py
@@ -0,0 +1,19 @@
+from DBManager import * 
+from ProtestChicagoParser import *
+
+class MainProtestFinder:
+	def __init__(self, db_name):
+		self.db = DBManager(db_name)
+		self.parsers = [ProtestChicagoParser(db_manager=self.db)]
+
+	def do_parses(self):
+		for parser in self.parsers:
+			parser.parse()
+
+	def export_json(self, path):
+		self.db.save_json(path)
+
+if __name__ == "__main__":
+	mpf = MainProtestFinder("files/protests.db")
+	mpf.do_parses()
+	mpf.export_json("files/protestjson.json")
---
+++ ProtestChicagoParser.py
@@ -0,0 +1,94 @@
+"""
+Changelog/ notes log
+	- Ok full disclosure: this uses no natural language processing, and is so hard coded
+	that if they change nearly any of their HTML tags this could break. With that said,
+	however, as of June 4 2020 file is working 
+	- Created file
+~ Jackson
+
+"""
+
+from DBManager import * 
+import feedparser
+from bs4 import BeautifulSoup
+import requests
+
+class ProtestChicagoParser:
+	def __init__(self, db_file="temp.db", db_manager=None):
+		self.url = "https://www.protestchicago.com/"
+		self.db_file = db_file
+
+		if db_manager is None:
+			self.db_manager = DBManager(self.db_file)
+		else:
+			self.db_manager = db_manager
+
+	#ALL PARSERS SHOULD HAVE A .parse() that does the parsing, and saves to the DB. Can or cannot have lat long implementation
+	def parse(self): 
+		# parse does the stiring of the beautiful soup to find the needed info
+		# this is probably going to break near instantly, so beware!
+
+        # protest takes (name, time_info, location, latitude, longitude, url, notes)
+		self.db_manager.create_connection()
+
+		found_count, added_count = 0, 0
+
+		url = self.url
+
+		try:
+		# Paginate through each page of the site, using url as a flag
+			while url is not None: 
+				headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36, accept-language: en-US,en'}
+
+				# while url not None:
+				page = requests.get(url=url, headers=headers)
+				soup = BeautifulSoup(page.text, 'html.parser')
+				articles = soup.find_all('article')
+
+				for article in articles:
+					found_count += 1
+
+					# Isolate the h1 tag, extract title
+					title_h1 = article.find('h1')
+					title = title_h1.get_text()
+
+					time_info = article.find('h2').get_text()
+
+					location = article.find('h3').get_text()
+					location = location.split("\n")
+					location = location[0]
+
+					# isolate the link from the h1 a tag
+					found_url = title_h1.find(href=True)['href']
+
+					# notes are probably in the p-tag
+					notes = article.find('p').get_text()
+
+					if not self.db_manager.check_val_in(str(found_url), "url"): # If the value isn't in the field already
+						added_count += 1
+						protest = (title, time_info, location, 0.0, 0.0, found_url, notes)
+						self.db_manager.create_protest(protest)
+					else:
+						continue
+						# print("skip")
+
+				# Pagination code; search for a tag containing next page number
+				# a tag with class "next page-numbers" is only generated when on a non-final page
+				next_page = soup.find_all('a', class_="next page-numbers")
+				if len(next_page) > 0:
+					url = next_page[0]['href']
+					print(f"Next page!!! Going to {url}")
+				else:
+					url = None
+
+			self.db_manager.close_connection()
+			return (found_count, added_count)
+
+		except Error as e:
+			print("ERROR IN ProtestChicagoParser/parse")
+			print(e)
+			
+if __name__ == "__main__":
+	pc = ProtestChicagoParser("files/protests.db")
+	a = pc.parse()
+	print(f"Located {a[0]} potential protests, added {a[1]} on Protest Chicago")
