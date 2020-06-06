7780617ce1324db5d0f6cf118efdedcac2907f5c
mfosterw <15671892+mfosterw@users.noreply.github.com>
2020-06-06 15:17:27 -0500

    Added default location so nothing is at (0, 0)

--- scripts/LocationManager.py
+++ scripts/LocationManager.py
@@ -4,7 +4,6 @@ Changelog/ notes log
 Jackson, June 6, 2020
 - I didn't test this at all
 - Used foster's presumably working code
-- PLEASE TEST BEFORE MERGING TO MASTER
 """
 
 import googlemaps
@@ -20,22 +19,25 @@ class LocationManager:
 		else:
 			self.gmaps = googlemaps.Client(key=self.api_key)
 
-	def add_location_data(self, db_manager):
+	def add_location_data(self, db_manager, default=(41.8781, -87.6298)):
 		"""
 			Goes through db_manager's db and adds location_data to all that it can
-
-			Note to dev team: I haven't tested this yet bc i don't have api key.
 		"""
 
 		count = 0
+		coords = dict()
 		next_up = db_manager.get_next_empty_loc()
 		while next_up is not None:
-			coords = dict()
-			code_res = self.gmaps.geocode(location)
-			print('Location results:', len(code_res))
-			coords = code_res[0]['geometry']['location']
+			location = next_up[1]
+			if location:
+				raise SystemExit('No spending money')
+				code_res = self.gmaps.geocode(location)
+				print('Location results:', len(code_res))
+				coords = code_res[0]['geometry']['location']
 
-			lat, lon = coords['lat'], coords['lng']
+				lat, lon = coords['lat'], coords['lng']
+			else:
+				lat, lon = default
 
 			db_manager.update_location(next_up[0], lat, lon)
 			next_up = db_manager.get_next_empty_loc()
